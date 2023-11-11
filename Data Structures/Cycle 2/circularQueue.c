f71c57861e2a3997face61e09996afb2394261ed
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2023-11-11 18:54:53 +0530

    Add files via upload

---
+++ Data Structures/Cycle 2/circularQueue.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#define MAX 5
+int queue[MAX], front = -1, rear = -1;
+
+void enqueue(){
+    int item;
+    printf("\nEnter The Element: ");
+    scanf("%d", &item);
+
+    if (front == - 1){
+        front = rear = 0;
+        queue[rear] = item;
+    }
+    else{
+        if ((rear+1) % MAX != front){
+            rear = (rear+1) % MAX;
+            queue[rear] = item;
+        }
+        else
+            printf("\nQueue Full!\n");
+    }
+}
+
+void dequeue(){
+    if (front == - 1)
+        printf("\nQueue Empty!\n");
+
+    else{
+        printf("\nDeleted Value: %d", queue[front]);
+
+        if(front == rear)
+            front = rear = -1;
+        else
+            front = (front + 1) % MAX;
+    }
+    
+}
+
+void display(){
+    if (front == -1)
+        printf("\nQueue Empty!\n");
+    else{
+        int tempf = front;
+        printf("\nQueue:\t");
+        while (tempf != rear){
+            printf("%d\t", queue[tempf]);
+            tempf = (tempf + 1) % MAX;
+        }
+        printf("%d\n", queue[tempf]);
+    }
+}
+
+void main(){
+    int menuFlag = 1;
+	
+    while (menuFlag == 1){
+        printf("\n\t\t ============= MENU =============");
+        printf("\n\t\t 1) Enqueue");
+        printf("\n\t\t 2) Dequeue");
+        printf("\n\t\t 3) Display");
+        printf("\n\t\t 4) Exit");
+        printf("\n\t\t ================================\n");
+  
+        int o;
+        printf("\nEnter Your Option: ");
+        scanf("%d", &o);
+  
+        switch(o){
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("\nClosing...\n");
+                menuFlag = 0;
+                break;
+            default:
+                printf("\nPlease Enter A Valid Option!\n");
+        }
+    }
+}
