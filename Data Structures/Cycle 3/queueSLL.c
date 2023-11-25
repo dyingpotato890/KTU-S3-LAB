5bf888dbcd7c7712472b66143c5d2b7e06657c05
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2023-11-25 23:48:24 +0530

    Add files via upload

---
+++ Data Structures/Cycle 3/queueSLL.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+struct node *front, *rear, *new, *ptr;
+
+struct node* createNode(){
+    int d;
+    printf("\nEnter The Data: ");
+    scanf("%d", &d);
+
+    struct node *temp = (struct node*)malloc(sizeof(struct node*));
+    temp -> data = d;
+    temp -> link = NULL;
+    return temp;
+}
+
+void enqueue(){  
+    new = createNode();
+
+    if (front != NULL){
+    	rear -> link = new;    	
+    	rear = new;
+    }
+    else
+    	front = rear = new;
+
+    printf("\nItem Inserted!");
+}
+
+void dequeue(){
+	if (front == NULL)
+		printf("\nQueue Empty!");
+	else{
+		if (front -> link == NULL){
+			printf("Dequeued Item: %d", front -> data);
+			front = rear = NULL;
+			free(front);
+			free(rear);
+		}
+		else{
+			ptr = front;
+			printf("Dequeued Item: %d", ptr -> data);
+			front = ptr -> link;
+			free(ptr);
+		}
+	}
+}
+
+void display(){
+	if (front == NULL)
+        printf("\nQueue Empty!");
+    else{
+        ptr = front;
+        printf("\nQueue Data: \n");
+
+        while(ptr != NULL){
+            printf("%d\t", ptr -> data);
+            ptr = ptr -> link;
+        }
+    }
+}
+
+void main(){
+    int menuFlag = 1;
+	
+    while (menuFlag == 1){
+        printf("\n\t\t ============= MENU =============");
+        printf("\n\t\t 1) Enqueue");
+        printf("\n\t\t 2) Dequeue");
+        printf("\n\t\t 3) Display");
+        printf("\n\t\t 4) Exit");
+        printf("\n\t\t ================================\n");
+  
+        int o;
+        printf("\nEnter Your Option: ");
+        scanf("%d", &o);
+  
+        switch(o){
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("\nClosing...\n");
+                menuFlag = 0;
+                break;
+            default:
+                printf("\nPlease Enter A Valid Option!\n");
+        }
+    }
+}
+
+/*
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 1
+
+Enter The Data: 10
+
+Item Inserted!
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 1
+
+Enter The Data: 20
+
+Item Inserted!
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 1
+
+Enter The Data: 30
+
+Item Inserted!
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 3
+
+Queue Data: 
+10	20	30	
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 2
+Dequeued Item: 10
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 2
+Dequeued Item: 20
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 2
+Dequeued Item: 30
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 
+3
+
+Queue Empty!
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 2
+
+Queue Empty!
+		 ============= MENU =============
+		 1) Enqueue
+		 2) Dequeue
+		 3) Display
+		 4) Exit
+		 ================================
+
+Enter Your Option: 4
+
+Closing...
+*/
