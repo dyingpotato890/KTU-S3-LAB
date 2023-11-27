55faf3a6487097544250619f8f5cad089e66fab0
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2023-11-27 21:34:09 +0530

    Add files via upload

---
+++ OOP Using Java/FileExceptions.java
@@ -0,0 +1,46 @@
+/*
+write a java program to read formn a file and write to a file by handling all the file related exceptions. 
+*/
+
+import java.io.*;
+
+class FileExceptions {
+    static void outputFile(){
+        try{
+            FileOutputStream out = new FileOutputStream("ReadME.txt");
+            String str = "Hello World!";
+            byte b[] = str.getBytes();
+            out.write(b);
+            out.close();
+            System.out.println("Content Stored!");
+        }
+        catch(java.lang.Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    static void inputFile(){
+        try{
+            FileInputStream in = new FileInputStream("ReadME.txt");
+            int i = 0; 
+            System.out.println("File Contents:\n");
+            while((i = in.read()) != -1)    
+                System.out.print((char)i);        
+            in.close();
+        }
+        catch(java.lang.Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String arg[]){
+        outputFile();
+        inputFile();
+    } 
+}
+
+/*
+Content Stored!
+File Contents:
+
+Hello World!
+ */
