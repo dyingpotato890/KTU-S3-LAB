b37c992de1600a8c81e970b93d1c76aee1353196
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2023-12-21 23:38:35 +0530

    Add files via upload

---
+++ Data Structures/Cycle 3/polySLL.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct term {
+    int coeff;
+    int expo;
+    struct term* link;
+};
+
+struct term *p1, *p2, *r, *pro;
+
+struct term* polyInit(){
+	int n, ctemp, etemp;  
+	printf("\nEnter The Number Of Polynomial Terms: ");
+	scanf("%d", &n);
+
+    struct term* ptr = (struct term*)malloc(sizeof(struct term));
+    struct term *head = ptr;
+
+	for (int i = 1; i < n+1; i++){
+        printf("\nEnter The Coefficient For T%d: ", i);
+        scanf("%d", &ctemp);
+        printf("Enter The Exponent For T%d: ", i);
+        scanf("%d", &etemp);
+		
+        ptr -> coeff = ctemp;
+        ptr -> expo = etemp;
+
+        if(i != n){
+			ptr -> link = (struct term*)malloc(sizeof(struct term));
+			ptr = ptr -> link;
+		}
+		else
+			ptr -> link = NULL;
+	}
+    return head;
+}
+
+void display(struct term* ptemp){
+    struct term* ptr = ptemp;
+
+	if (ptr == NULL)
+        printf("Polynomial Does Not Exist!");
+    else{
+        while(ptr != NULL){
+		    if(ptr -> expo != 0)
+			    printf("%dx^%d", ptr-> coeff, ptr -> expo);
+		    else
+			    printf("%d", ptr -> coeff);
+		    if(ptr -> link != NULL)
+			    printf(" + ");
+		    ptr = ptr -> link;
+	    }
+    }
+}
+
+void add(){
+	struct term *ptr1, *ptr2, *ptrR, *ptrr;
+	ptr1 = p1;
+	ptr2 = p2;
+    ptrR  = (struct term*)malloc(sizeof(struct term));
+    r = ptrR;
+	
+	while (ptr1 != NULL && ptr2 != NULL) {
+        if ((ptr1 -> expo) < (ptr2 -> expo)) {
+            ptrR -> coeff = ptr2 -> coeff;
+            ptrR -> expo = ptr2 -> expo;
+            ptr2 = ptr2 -> link;
+        }
+        else if ((ptr1 -> expo) > (ptr2 -> expo)) {
+            ptrR -> coeff = ptr1 -> coeff;
+            ptrR -> expo = ptr1 -> expo;
+            ptr1 = ptr1 -> link;
+        }
+        else{
+			if(ptr1 -> coeff + ptr2 -> coeff != 0){
+				ptrR -> coeff = ptr1 -> coeff + ptr2 -> coeff;
+				ptrR -> expo = ptr1 -> expo;
+				ptr1 = ptr1 -> link;
+				ptr2 = ptr2 -> link;
+			}
+			else{
+				ptr1 = ptr1 -> link;
+				ptr2 = ptr2 -> link;
+				continue;
+			}
+		}
+        ptrR -> link = (struct term*)malloc(sizeof(struct term));
+		ptrR = ptrR -> link;
+    }
+    
+    if (ptr1 != NULL)
+		ptrr = ptr1;
+	else
+		ptrr = ptr2;
+
+	while(ptrr != NULL){
+		ptrR -> coeff = ptrr -> coeff;
+		ptrR -> expo = ptrr -> expo;
+
+		if(ptrr -> link != NULL){
+			ptrR -> link = (struct term*)malloc(sizeof(struct term));
+			ptrR = ptrR -> link;
+		}
+		else
+			ptrR -> link = NULL;
+		ptrr = ptrr -> link;
+	}
+}
+
+void mult() {
+    struct term *p = p1, *q = p2, *r = pro, *new, *prev;
+    while (p != NULL) {
+        while (q != NULL) {
+            new = (struct term*)malloc(sizeof(struct term));
+            new -> coeff = (p -> coeff) * (q -> coeff);
+            new -> expo = (p -> expo) + (q -> expo);
+            new -> link = NULL;
+            if (pro == NULL) 
+                pro = r = new;
+			else{
+                r -> link = new;
+                r = new;
+            }
+            q = q -> link;
+        }
+        p = p -> link;
+        q = p2;
+    }
+    p = pro;
+    while (p != NULL) {
+        prev = p, q = p -> link;
+        while (q != NULL) {
+            if (p -> expo == q -> expo){
+                p -> coeff = (p -> coeff) + (q -> coeff);
+                prev -> link = q -> link;
+                free(q);
+                q = prev -> link;
+            } 
+			else{
+                prev = q;
+                q = q -> link;
+            }
+        }
+        p = p -> link;
+    }
+}
+
+void main(){
+	printf("\n======= Inititializing P1 =======");
+	p1 = polyInit();   
+	printf("\n======= Inititializing P2 =======");
+	p2 = polyInit();
+
+    printf("\nPolynomial 1: ");
+    display(p1);
+    printf("\nPolynomial 2: ");
+    display(p2);
+
+    add();
+    printf("\nSum: ");
+    display(r);
+
+	mult();
+    printf("\nProduct: ");
+    display(pro);
+}
