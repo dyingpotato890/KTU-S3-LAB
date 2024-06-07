e320eebf18d2cefab9776174fdf095dafdf605a7
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2024-06-07 10:09:56 +0530

    Updated priorityQueue.c

--- Data Structures/Cycle 2/priorityQueue.c
+++ Data Structures/Cycle 2/priorityQueue.c
@@ -1,65 +1,64 @@
 #include <stdio.h>
 
 struct queue {
-	int data;
+    int data;
     int priority;
-}
-q[100];
+} q[100];
 
 int front = -1, rear = -1, size;
 
-void enqueue(int d, int p){
-    if (rear == size - 1) 
+void enqueue(int d, int p) {
+    if (rear == size - 1) {
         printf("Queue is full\n");
-    else {
+    } else {
         if (front == -1) {
             front = 0;
-            q[++rear].data = d;
-            q[rear].priority = p;
-        } else {
-            struct queue temp;
-            q[++rear].data = d;
-            q[rear].priority = p;
-            for (int i = rear; i > front; i--) {
-                if (q[i].priority < q[i - 1].priority) {
-                    temp = q[i];
-                    q[i] = q[i - 1];
-                    q[i - 1] = temp;
-                }
-            }
+        }
+        rear++;
+        q[rear].data = d;
+        q[rear].priority = p;
+
+        int i = rear;
+        while (i > front && q[i].priority < q[i - 1].priority) {
+            struct queue temp = q[i];
+            q[i] = q[i - 1];
+            q[i - 1] = temp;
+            i--;
         }
     }
 }
 
 void dequeue() {
-    if (front == -1)
+    if (front == -1) {
         printf("Queue is empty\n");
-    else {
+    } else {
         printf("Deleted element: %d\n", q[front].data);
-        if (front == rear)
+        if (front == rear) {
             front = rear = -1;
-        else
+        } else {
             front++;
+        }
     }
 }
 
 void display() {
-    if (front == -1)
-        printf("\nQueue: Empty Queue\n"); 
-    else {
+    if (front == -1) {
+        printf("\nQueue: Empty Queue\n");
+    } else {
         printf("\nQueue: ");
-        for (int i = front; i <= rear; i++)
-            printf("%d\t", q[i].data);
+        for (int i = front; i <= rear; i++) {
+            printf("%d(p%d)\t", q[i].data, q[i].priority);
+        }
         printf("\n");
     }
 }
 
 void main() {
-    int o, d, p;
+    int o = 0, d, p;
     printf("Enter The Size of The Queue: ");
     scanf("%d", &size);
 
-	printf("\n\t\t ============= MENU =============");
+    printf("\n\t\t ============= MENU =============");
     printf("\n\t\t 1) Enqueue");
     printf("\n\t\t 2) Dequeue");
     printf("\n\t\t 3) Exit");
@@ -68,22 +67,22 @@ void main() {
     while (o != 3) {
         display();
 
-		printf("\nEnter Your Option: ");
-        scanf("%d", & o);
+        printf("\nEnter Your Option: ");
+        scanf("%d", &o);
         switch (o) {
-        case 1:
-            printf("Enter The Data And Priority: ");
-            scanf("%d %d", & d, & p);
-            enqueue(d, p);
-            break;
-        case 2:
-            dequeue();
-            break;
-        case 3:
-            printf("Closing...\n");
-            break;
-        default:
-            printf("Invalid choice!\n");
+            case 1:
+                printf("Enter The Data And Priority: ");
+                scanf("%d %d", &d, &p);
+                enqueue(d, p);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                printf("Closing...\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
         }
     }
 }
